e860989f8961dc34171f2eca0dcbf87abef3b53c
dagregi <120724828+dagregi@users.noreply.github.com>
2024-04-10 16:20:57 +0300

    changed the fade to typing animation

--- main.js
+++ main.js
@@ -1,15 +1,34 @@
 const job_titles = [
     "System Level Programmer",
     "Fullstack Developer",
-	"Linux Enthusiast",
-	"Developer Workflow Optimizer",
+    "Linux Enthusiast",
+    "Developer Workflow Optimizer",
 ];
 const header = document.getElementById("job_title");
 let index = 0;
+let char_index = 0;
+let is_deleting = false;
 
-function cycle_job_titles() {
-    header.textContent = job_titles[index];
-    index = (index + 1) % job_titles.length;
+function char_delete() {
+    const current_text = job_titles[index];
+    if (!is_deleting) {
+        header.textContent = current_text.substring(0, char_index + 1);
+        char_index++;
+    } else {
+        header.textContent = current_text.substring(0, char_index - 1);
+        char_index--;
+    }
+
+    if (!is_deleting && char_index === current_text.length) {
+        is_deleting = true;
+        setTimeout(char_delete, 500);
+    } else if (is_deleting && char_index === 1) {
+        is_deleting = false;
+        index = (index + 1) % job_titles.length;
+        setTimeout(char_delete, 150);
+    } else {
+        setTimeout(char_delete, 50);
+    }
 }
 
-setInterval(cycle_job_titles, 3000);
+char_delete();
