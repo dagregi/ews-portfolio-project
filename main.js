0e3243d5f5521aa7cbe7d6063c841e7b0b891272
dagregi <120724828+dagregi@users.noreply.github.com>
2024-04-16 14:38:07 +0300

    fixed redundant code

--- main.js
+++ main.js
@@ -8,43 +8,35 @@ const header = document.getElementById("job_title");
 let index = 0;
 let char_index = 0;
 let is_deleting = false;
-
 let current_command = "";
 let current_output_id = "";
 
 function run_command(command) {
-    if (current_command === command) {
-        document.getElementById("typed_command").textContent = "";
-        document
-            .querySelectorAll(".output")
-            .forEach((output) => (output.style.display = "none"));
-    } else {
-        current_command = command;
-        current_output_id = command + "_output";
-        document.getElementById("typed_command").textContent = "";
-        document
-            .querySelectorAll(".output")
-            .forEach((output) => (output.style.display = "none"));
-        type_command(command);
-    }
+    current_command = command;
+    current_output_id = command + "_output";
+    clear_command();
+    type_command(command);
+}
+
+function clear_command() {
+    document.getElementById("typed_command").textContent = "";
+    document
+        .querySelectorAll(".output")
+        .forEach((output) => (output.style.display = "none"));
 }
 
 function type_command(command) {
     let index = 0;
-    const command_text = "cat " + command + ".txt ";
+    const command_text = command;
     const interval = setInterval(() => {
         document.getElementById("typed_command").textContent +=
             command_text[index];
         index++;
         if (index === command_text.length) {
             clearInterval(interval);
-            display_output();
+            document.getElementById(current_output_id).style.display = "block";
         }
     }, 50);
-}
-
-function display_output() {
-    document.getElementById(current_output_id).style.display = "block";
 }
 
 function char_delete() {
@@ -56,7 +48,6 @@ function char_delete() {
         header.textContent = current_text.substring(0, char_index - 1);
         char_index--;
     }
-
     if (!is_deleting && char_index === current_text.length) {
         is_deleting = true;
         setTimeout(char_delete, 500);
